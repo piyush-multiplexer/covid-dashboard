015b63421df68479529bd7ad6646f1423799d870
Piyush Goyani <piyush.goyani@ezeetechnosys.com>
2021-04-21 12:35:13 +0530

    highchart added and remove d3,

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,20 +13,17 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-    'element-ui/lib/theme-chalk/index.css'
-  ],
+  css: ['element-ui/lib/theme-chalk/index.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     '@/plugins/CompositionAPI',
-    '@/plugins/element-ui'
+    '@/plugins/element-ui',
+    { src: '@/plugins/vue-highcharts.js', ssr: false }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
