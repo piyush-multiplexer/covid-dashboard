ff10a693bd6594be7049b7bc2356c07467f072a0
Piyush Goyani <piyush.goyani@ezeetechnosys.com>
2021-04-22 09:00:30 +0530

    quasar configuration and related files

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,66 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <q-layout view="lHh Lpr lFf">
+    <q-page-container>
+      <country-historical />
+        <multi-line-chart />
+      <el-card>
+        <el-row slot="header" class="clearfix">
+          <el-col :span="12">
+            <span>COVID Overall Data (India)</span>
+          </el-col>
+        </el-row>
+        <el-row>
+          <el-col class="ml-2" :span="9">
+            <el-input v-model="search" placeholder="Search by state" />
+          </el-col>
+          <el-col :span="3">
+            <el-button @click="test">
+              Get Data
+            </el-button>
+          </el-col>
+        </el-row>
+        <el-table v-if="result" :search="search" :data="result.states">
+          <el-table-column
+            v-for="(column,index) in stateHeaders"
+            :key="index"
+            :prop="column.value"
+            :label="column.text"
+            :width="column.width"
+          />
+        </el-table>
+      </el-card>
+    </q-page-container>
+  </q-layout>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+import { defineComponent, ref } from '@vue/composition-api'
+import CountryHistorical from '~/components/CountryHistorical.vue'
+import MultiLineChart from '~/components/MultiLineChart.vue'
+let self
+export default defineComponent({
+  components: { MultiLineChart, CountryHistorical },
+  setup () {
+    const result = ref(null)
+    const search = ref(null)
+    const stateHeaders = [
+      { text: 'State', value: 'state', width: 350 },
+      { text: 'Cases', value: 'cases', width: 100 },
+      { text: 'Active', value: 'active', width: 100 },
+      { text: 'Deaths', value: 'deaths', width: 100 },
+      { text: 'Recovered', value: 'recovered', width: 100 }
+    ]
+    function test () {
+      self.$axios.get('https://disease.sh/v3/covid-19/gov/India').then((res:any) => {
+        // console.log(res.data)
+        result.value = res.data
+        // console.log(result)
+      })
+    }
+    return { test, result, stateHeaders, search }
+  },
+  created () {
+    self = this
   }
-}
+})
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
