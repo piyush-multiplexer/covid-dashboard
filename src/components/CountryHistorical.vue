53d005078d4cc754f8c26e2d9bcf3931c13862a3
Piyush Goyani <piyush.goyani@ezeetechnosys.com>
2021-04-23 15:40:25 +0530

    world historiacal data in chart added for covid

--- src/components/CountryHistorical.vue
+++ src/components/CountryHistorical.vue
@@ -11,31 +11,44 @@ export default defineComponent({
   setup() {
     onMounted(() => {
       axios
-        .get("https://disease.sh/v3/covid-19/historical/India?lastdays=all")
+        .get("https://disease.sh/v3/covid-19/historical/all?lastdays=all")
         .then(async (res) => {
           const config = {
             type: "line",
             data: {
-              labels: Object.keys(res.data.timeline.cases),
+              labels: Object.keys(res.data.cases),
               datasets: [
                 {
                   label: "Confirmed",
                   backgroundColor: "blue",
-                  data: Object.values(res.data.timeline.cases),
+                  data: Object.values(res.data.cases),
                 },
                 {
                   label: "Recovered",
                   backgroundColor: "green",
-                  data: Object.values(res.data.timeline.recovered),
+                  data: Object.values(res.data.recovered),
                 },
                 {
                   label: "Deaths",
                   backgroundColor: "red",
-                  data: Object.values(res.data.timeline.deaths),
+                  data: Object.values(res.data.deaths),
                 },
               ],
             },
             options: {
+              plugins: {
+                title: {
+                  display: true,
+                  text: "World COVID Timeseries Data",
+                  position: "top",
+                  align: "center",
+                  font: { weight: 700, size: 18 },
+                  padding: {
+                    top: 15,
+                    bottom: 5,
+                  },
+                },
+              },
               responsive: true,
               maintainAspectRatio: false,
               scales: {
@@ -58,7 +71,7 @@ export default defineComponent({
 </script>
 <style scoped>
 .chart-container {
-    width: 96vw;
-    height:600px
+  width: 96vw;
+  height: 600px;
 }
 </style>
