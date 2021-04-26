09ee646a6708492d0f8331f807ad9d083c9a0036
Piyush Goyani <piyush.goyani@ezeetechnosys.com>
2021-04-26 09:30:24 +0530

    show country specific covid data with pie chart distibution(watchEffect)

--- src/components/CountryCovidData.vue
+++ src/components/CountryCovidData.vue
@@ -2,31 +2,66 @@
   <div class="row">
     {{ covidData }}
     <div class="chart-container">
-      <canvas id="WorldVaccineCoverage"></canvas>
+      <canvas id="CountryCovidData-Pie"></canvas>
     </div>
     <div class="chart-container">
       <canvas id="WorldVaccineCoverage"></canvas>
     </div>
   </div>
 </template>
-<script lang="js">
-import { defineComponent, ref, reactive } from 'vue'
+<script>
+import Chart from "chart.js/auto";
+import { defineComponent, ref, reactive, watchEffect } from "vue";
 
 export default defineComponent({
-  props:['countryData'],
-  setup (_props) {    
-    let options = ref({})
-    let covidData = reactive(null)
-    console.log('props',_props.countryData);  
-    // eslint-disable-next-line vue/no-setup-props-destructure
-    covidData = _props.countryData
-    return { options,covidData }
-  }
-})
+  props: ["countryData"],
+  setup(_props) {
+    let options = ref({});
+    let covidData = reactive(_props.countryData);
+    watchEffect(() => {
+      console.log("wE ", _props.countryData);
+      covidData = _props.countryData;
+      const config = {
+        type: "pie",
+        data: {
+          labels: ["Confirmed", "Recovered", "Critical", "Deaths"],
+          datasets: [
+            {
+              label: "Dataset One",
+              backgroundColor: ["blue", "green", "orange", "red"],
+              data: [
+                covidData.cases,
+                covidData.recovered,
+                covidData.critical,
+                covidData.deaths,
+              ],
+            },
+          ],
+        },
+        options: {
+          responsive: true,
+          maintainAspectRatio: false,
+          plugins: {
+            legend: { position: "top" },
+            title: {
+              display: true,
+              text: `(${covidData.country}) COIVD Data with Population (${covidData.population}) Distribution`,
+              font: { weight: 700, size: 18 },
+            },
+          },
+        },
+      };
+      setTimeout(() => {
+        new Chart(document.getElementById("CountryCovidData-Pie"), config);
+      }, 500);
+    });
+    return { options, covidData };
+  },
+});
 </script>
 <style scoped>
 .chart-container {
-  width: 96vw;
-  height: 600px;
+  width: 40vw;
+  height: 400px;
 }
 </style>
