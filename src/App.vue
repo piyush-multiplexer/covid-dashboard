ea696b0e3b32a37c98e34e0e9d7bfe338e9d0973
Piyush Goyani <piyush.goyani@ezeetechnosys.com>
2021-04-28 14:27:57 +0530

    dashoard and server pagination two seperate screen made for router navigation and wra other component inside those, button for route navigation added

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,32 @@
 <template>
   <q-layout view="lHh Lpr lFf">
     <q-page-container class="q-pa-sm q-pb-md">
-      <country-historical />
-      <world-vaccine-coverage />
-      <covid-data-by-country @country-clicked="viewCountryData" />
-      <country-covid-data v-if="countryData" :country-data="countryData" />
+      <div class="q-pa-md q-gutter-sm">
+        <q-btn
+          dense
+          size="12"
+          outline
+          label="Dashboard"
+          color="primary"
+          to="/"
+        />
+        <q-btn
+          dense
+          size="12"
+          outline
+          label="Server Pagination"
+          color="primary"
+          to="server-pagination"
+        />
+      </div>
+      <router-view />
     </q-page-container>
   </q-layout>
 </template>
 
 <script>
-import { defineComponent, ref } from "vue";
-import CountryHistorical from "@/components/CountryHistorical.vue";
-import WorldVaccineCoverage from "./components/WorldVaccineCoverage.vue";
-import CovidDataByCountry from "./components/CovidDataByCountry.vue";
-import CountryCovidData from "./components/CountryCovidData.vue";
+import { defineComponent } from "vue";
 export default defineComponent({
-  components: {
-    CountryHistorical,
-    WorldVaccineCoverage,
-    CovidDataByCountry,
-    CountryCovidData,
-  },
-  setup() {
-    let countryData = ref(null);
-    function viewCountryData(param) {
-      countryData.value = param;
-    }
-    return { viewCountryData, countryData };
-  },
+  setup() {},
 });
 </script>
